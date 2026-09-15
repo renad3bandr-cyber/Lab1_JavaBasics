55e38068464be09914533feb470512ef023ba95f
Renad <renad3bandr@gmail.com>
2026-09-15 19:32:46 +0300

    Add HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, SE4111!");
-        System.out.println("Welcome to Git!");
+        System.out.println("Welcome to Software Construction");
+        System.out.println("This is my Git Lab!");
     }
 }
